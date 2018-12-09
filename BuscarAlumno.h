3039904171f5042733803c1a2dc64216cf7f44f5
David Bermejo Sánchez <i72besad@uco.es>
2018-12-09 12:51:31 +0100

    Subida BuscarAlumno.h

---
+++ BuscarAlumno.h
@@ -0,0 +1,12 @@
+#ifndef BUSCARALUMNO_H
+#define BUSCARALUMNO_H
+
+void BuscarAlumno();
+void BusquedaDNI();
+void BusquedaApellido();
+void Correcto(int opc2, string DNI);
+void Incorrecto(int opc2);
+
+
+
+#endif
