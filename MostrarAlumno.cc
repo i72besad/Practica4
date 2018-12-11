82c178e2d361095518bc617f0f2af1115e84db47
David Bermejo Sánchez <i72besad@uco.es>
2018-12-11 18:07:29 +0100

    Modificacion de MostrarAlumno.cc

--- MostrarAlumno.cc
+++ MostrarAlumno.cc
@@ -7,9 +7,14 @@ using namespace std;
 
 void MostrarAlumno(string DNI){
 
+	int opc;
+
+	alumno a;
+
 	cout<<"¿Qué desea ver?"<<endl;
-	cout<<"\n\t0-->Mostrar Todos   1-->Un Alumno"<<endl;
+	cout<<"\n\t0-->Mostrar Todos los Alumnos  1-->El Alumno"<<endl;
 	cin>>opc;
+	cin.ignore();
 	if(opc==0){
 		MostrarTodos();
 	}if(opc>1 || opc<0){
@@ -17,7 +22,7 @@ void MostrarAlumno(string DNI){
 			cout<<"Error, opción incorrecta"<<endl;
 			cout<<"Introduzca de nuevo una opción:\n"<<endl;		
 			cout<<"¿Qué desea ver?"<<endl;
-			cout<<"\n\t0-->Mostrar Todos   1-->Un Alumno"<<endl;				
+			cout<<"\n\t0-->Mostrar Todos los Alumnos   1-->El Alumno"<<endl;				
 			cin>>opc2;
 			if(opc==0){
 				MostrarTodos();
@@ -26,15 +31,15 @@ void MostrarAlumno(string DNI){
 	}if(opc==1){
 
 
-		cout<<"\n\n\tDNI--> "<<alumno.getDNI()<<endl;
-		cout<<"\n\n\tNombre--> "<<alumno.getNombre()<<endl;
-		cout<<"\n\n\tApellidos--> "<<alumno.getApellidos()<<endl;
-		cout<<"\n\n\tTelefono--> "<<alumno.getTelefono()<<endl;
-		cout<<"\n\n\tDireccion--> "<<alumno.getDireccion()<<endl;
-		cout<<"\n\n\temail--> "<<alumno.getEmail()<<endl;
-		cout<<"\n\n\tCurso mas alto--> "<<alumno.getCursoMasAlto()<<endl;
-		cout<<"\n\n\tLider--> "<<alumno.getLider()<<endl;
-		cout<<"\n\n\tEquipo--> "<<alumno.getEquipo()<<endl;
+		cout<<"\n\n\tDNI--> "<<a.getDNI()<<endl;
+		cout<<"\n\n\tNombre--> "<<a.getNombre()<<endl;
+		cout<<"\n\n\tApellidos--> "<<a.getApellidos()<<endl;
+		cout<<"\n\n\tTelefono--> "<<a.getTelefono()<<endl;
+		cout<<"\n\n\tDireccion--> "<<a.getDireccion()<<endl;
+		cout<<"\n\n\temail--> "<<a.getEmail()<<endl;
+		cout<<"\n\n\tCurso mas alto--> "<<a.getCursoMasAlto()<<endl;
+		cout<<"\n\n\tLider--> "<<a.getLider()<<endl;
+		cout<<"\n\n\tEquipo--> "<<a.getEquipo()<<endl;
 	}
 
 
