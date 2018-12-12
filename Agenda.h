ffb3ac793e677678d1be1a425c6a199956ef0e93
Jorge Jesús Chaparro Ibarra <jorgejezuh@gmail.com>
2018-12-12 13:18:55 +0100

    Subida de Agenda.h

---
+++ Agenda.h
@@ -0,0 +1,38 @@
+//Clase Agenda realizada por Jorge Jesús Chaparro Ibarra
+
+#ifndef AGENDA_H
+#define AGENDA_H
+
+#include <iostream>
+#include <string>
+#include <list>
+
+
+#include "alumno.h"
+
+using namespace std;
+
+class Agenda {
+private:
+	list <Alumno> alumnos_;
+	string usuario_;
+	string password_;
+public:
+	//Constructor de la Clase Agenda
+	Agenda(){usuario_=""; password_="";}
+
+	//get y set de la variable Usuario
+	inline string getUsuario(){return usuario_;}
+
+	//get y set de la variable contraseña
+	inline string getContra(){return password_;}
+
+	//Función Register
+	void mainmenu();
+	bool login();
+	void registerme();
+	void registerpassword(string usuario);
+	void writetofile(string usuario, string password);
+};
+
+#endif
