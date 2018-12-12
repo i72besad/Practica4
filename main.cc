9a617be01025200240210dbaad73c3fe3b0d85d0
David Bermejo Sánchez <i72besad@uco.es>
2018-12-12 12:09:57 +0100

    Subida de main.cc

---
+++ main.cc
@@ -0,0 +1,81 @@
+#include <iostream>
+#include "Agenda.h"
+
+
+
+
+int main(){
+
+	int opc,save;
+
+	mainmenu();
+
+	while(opc!=8){
+		cout<<"\nBienvenido a su Agenda de alumnos personal, ¿qué desea hacer?"<<endl;
+		cout<<"\n\t0--> Buscar Alumno"<<endl;
+		cout<<"\t1--> Insertar Alumno"<<endl;
+		cout<<"\t2--> Borrar Alumno"<<endl;
+		cout<<"\t3--> Modificar Alumno"<<endl;
+		cout<<"\t4--> Mostrar Alumno"<<endl;
+		cout<<"\t5--> Mostrar Lista de Alumnos"<<endl;
+		cout<<"\t6--> Gestionar Líderes de Equipo"<<endl;
+		cout<<"\t7--> Guardar"<<endl;
+		cout<<"\t8--> Salir"<<endl;
+		cin>>opc;
+		cin.ignore();
+		switch(opc){
+			case 0:
+				BuscarAlumno();
+				break;
+			case 1:
+				InsertarAlumno();
+				break;
+			case 2:
+				BorrarAlumno();
+				break;
+			case 3:
+				ModificarAlumno();
+				break;
+			case 4:
+				MostrarAlumno();
+				break;
+			case 5:
+				MostrarTodos();
+				break;
+			case 6:
+				GestionarLideres();
+				break;
+			case 7:
+				GuardarCopia();
+				break;		
+			case 8:
+				cout<<"¿Quiere guardar?"<<endl;
+				cout<<"0--> Sí   1-->No"<<endl;
+				cin-->save;
+				cin.ignore();
+				if(save==0){
+					GuardarCopia();
+					CargarCopia();
+				}if(save>1 || save<0){
+					
+					while(save>1 || save<0){
+						cout<<"Error, escriba de nuevo la opción"<<endl;
+						cout<<"¿Quiere guardar?"<<endl;
+						cout<<"0--> Sí   1-->No"<<endl;
+						cin>>save;
+						cin.ignore();
+						if(save==0){
+							GuardarCopia();
+							CargarCopia();
+						}
+					}
+
+				}
+				break;
+
+		}
+	}
+
+
+	return 0;
+}
