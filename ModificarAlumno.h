e0cfe94299d32e8cde0b097bc8d2316f52380b4b
Jorge Jesús Chaparro Ibarra <jorgejezuh@gmail.com>
2018-12-13 18:46:15 +0100

    Subida de modificar Alumno.h

---
+++ ModificarAlumno.h
@@ -0,0 +1,13 @@
+#ifndef MODIFICARALUMNO_H
+#define MODIFICARALUMNO_H
+
+#include "alumno.h"
+#include "Buscar_Alumno.h"
+
+#include <iostream>
+#include <string>
+#include <list>
+
+list <Alumno> ModificaAlumno();
+
+#endif
