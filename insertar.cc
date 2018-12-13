a3949223897daa43fded57fa21f0434ce37283a6
i52jilue <i52jilue@uco.es>
2018-12-13 19:55:51 +0100

    Correccion insertar.cc

--- insertar.cc
+++ insertar.cc
@@ -1,72 +1,70 @@
-#include<stdlib.h>
-#include<string.h>
-#include<fstream>
-#include"insertar.h"
+#include "alumno.h"
+#include "agenda.h"
+
+#include <iostream>
+#include <string>
+#include <list>
+
+
 
 void insertar(){
 	
-	ofstream archivo;
-	string nombreFich,dni,nombre,apellidos,email,direccion,nacimiento;
-	int telefono,postal,cursomax,equipo;
-	bool lider;
-	
-	std::cout<<"Nombre fichero:";
-	getline(cin,nombreFich);	
-	
-	archivo.open(nombreFich.c_str(),ios::app);
-	
+	Alumno al;
 	
 
 	cout << "DNI:";
 	cin>>dni;
+	al.setDNI(dni);
 	
 	cout << "Nombre:";
 	cin>>nombre;
+	al.setNombre(nombre);
+
 	
 	cout << "Apellidos:";
 	cin>>apellidos;
+	al.setApellidos(apellidos);
+
 	
 	cout << "Teléfono:";
 	cin>>telefono;
+	al.setTelefono(telefono);
+
 
 	cout << "Dirección:";
 	cin>>direccion;
+	al.setDireccion(direccion);
 
 	cout << "Codigo Postal:";
 	cin>>postal;
+	al.setPostal(postal);
 
 	cout << "Fecha de nacimiento:";
 	cin>>nacimiento;
-
+	al.setNacimiento(nacimiento);
+	
 	cout << "Email:";
 	cin>>email;
+	al.setEmail(email);
 
 	cout << "Curso más Alto matriculado:";
 	cin>>cursomax;
+	al.setCursoMasAlto(cursomax);
 
 	cout << "Número de equipo:";
 	cin>>equipo;
+	al.setEquipo(equipo);
 
 	cout << "Líder de equipo: ";	
 	cin>>lider;
+	al.setlider(lider);
 
 	
-	archivo<<dni<<endl;
-	archivo<<nombre<<endl;
-	archivo<<apellidos<<endl;
-	archivo<<telefono<<endl;
-	archivo<<direccion<<endl;
-	archivo<<postal<<endl;
-	archivo<<nacimiento<<endl;
-	archivo<<email<<endl;
-	archivo<<cursomax<<endl;
-	archivo<<equipo<<endl;
-	archivo<<lider<<endl;
-	
+
+	alumnos_.push_back(al);
 
 
-	archivo.close();
-	
+
 	
 }
 
