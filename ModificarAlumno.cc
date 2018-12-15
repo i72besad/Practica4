462eaf985dbd02350db3e87cc329932c55fbdc42
Jorge Jesús Chaparro Ibarra <jorgejezuh@gmail.com>
2018-12-15 14:46:43 +0100

    Actualización ModificarAlumno.cc

--- ModificarAlumno.cc
+++ ModificarAlumno.cc
@@ -5,33 +5,29 @@
 
 #include <iostream>
 #include <string>
-#include <list>
+#include <vector>
 
-
-
-list <Alumno> ModificaAlumno(){
+void ModificaAlumno(){
 
 	string auxs;
 	int auxi;
-	list <Alumno> clase = BuscarAlumnos();
-
-	for(list <Alumno>::iterator it=clase.begin(); it!=clase.end(); it++){
+	int pos = BuscarAlumnos();
 
 		int opcion;
 
 		do {
 			cout << "Elige el atributo del Alumno que desea modificar: \n" << endl;
 			
-			cout << "1. DNI:" << it->getDNI() << "\n" << endl;
-			cout << "2. Nombre:" << it->getNombre() << "\n" << endl;
-			cout << "3. Apellidos:" << it->getApellidos() << "\n" << endl;
-			cout << "4. Teléfono:" << it->getTelefono() << "\n" <<endl;
-			cout << "5. Dirección:" << it->getDireccion() << "\n" << endl;
-			cout << "6. eMail:" << it->getEmail() << "\n" << endl;
-			cout << "7. Curso más Alto matriculado:" << it->getCursoMasAlto() << "\n" << endl;
-			cout << "8. Número de equipo:" << it->getEquipo() << "\n" << endl;
+			cout << "1. DNI:" << alumnos_[pos].getDNI() << "\n" << endl;
+			cout << "2. Nombre:" << alumnos_[pos].getNombre() << "\n" << endl;
+			cout << "3. Apellidos:" << alumnos_[pos].getApellidos() << "\n" << endl;
+			cout << "4. Teléfono:" << alumnos_[pos].getTelefono() << "\n" <<endl;
+			cout << "5. Dirección:" << alumnos_[pos].getDireccion() << "\n" << endl;
+			cout << "6. eMail:" << alumnos_[pos].getEmail() << "\n" << endl;
+			cout << "7. Curso más Alto matriculado:" << alumnos_[pos].getCurso() << "\n" << endl;
+			cout << "8. Número de equipo:" << alumnos_[pos].getNequipo() << "\n" << endl;
 			cout << "9. Líder de equipo: ";
-			if(it->getLider()==true){
+			if(alumnos_[pos].getLider()==true){
 				cout << "Sí" << endl;
 			}else{
 				cout << "No" << endl;
@@ -44,54 +40,54 @@ list <Alumno> ModificaAlumno(){
 			switch (opcion) {
 				case '1':
 					cout << "Introduce el nuevo DNI:  \n";
-					cin >> auxs;
-					(*it).setDNI(auxs);
+					getline(cin, auxs);
+					alumnos_[pos].setDNI(auxs);
 					break;
 
 				case '2':
 					cout << "Introduce el nuevo Nombre:  \n";
-					cin >> auxs;
-					(*it).setNombre(auxs);
+					getline(cin, auxs);
+					alumnos_[pos].setNombre(auxs);
 					break;
 
 				case '3':
 					cout << "Introduce los nuevos Apellidos: \n";
-					cin >> auxs;
-					(*it).setApellidos(auxs);
+					getline(cin, auxs)
+					alumnos_[pos].setApellido(auxs);
 					break;
 
 				case '4':
 					cout << "Introduce el nuevo Teléfono: \n";
 					cin >> auxi;
-					(*it).setTelefono(auxi);
+					alumnos_[pos].setTelefono(auxi);
 					break;
 
 				case '5':
 					cout << "Introduce la nueva Dirección: \n";
-					cin >> auxs;
-					(*it).setDireccion(auxs);
+					getline(cin, auxs);
+					alumnos_[pos].setDireccion(auxs);
 					break;
 
 				case '6':
 					cout << "Introduce el nuevo eMail: \n";
-					cin >> auxs;
-					(*it).setEmail(auxs);
+					getline(cin, auxs);
+					alumnos_[pos].setEmail(auxs);
 					break;
 
 				case '7':
 					cout << "Introduce el nuevo Curso más Alto: \n";
 					cin >> auxi;
-					(*it).setCursoMasAlto(auxi);
+					alumnos_[pos].setCurso(auxi);
 					break;
 
 				case '8':
 					cout << "Introduce su nuevo equipo: \n";
 					cin >> auxi;
-					(*it).setEquipo(auxi);
+					alumnos_[pos].setNequipo(auxi);
 					break;
 
 				case '9':
-					(*it).cambiaLider();
+					alumnos_[pos].cambiaLider();
 					break;
 
 				case '0':
@@ -99,10 +95,7 @@ list <Alumno> ModificaAlumno(){
 					break;
 
 				default: 
-					cout <<opcion << "La opción introducida no está dentro del Menú.\n" << endl;
+					cout << opcion << "La opción introducida no está dentro del Menú.\n" << endl;
 			}
 		}while (opcion > 0 );
-
-	}
-	return clase;
 }
