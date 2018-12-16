47758568b592607f0bb9f906631aef145261a103
Jorge Jesús Chaparro Ibarra <jorgejezuh@gmail.com>
2018-12-16 12:58:25 +0100

    Mejoras en Agenda.cc

--- Agenda.cc
+++ Agenda.cc
@@ -505,3 +505,59 @@ void Agenda::eliminarAlumno()
 
 
 }
+
+//Funciones de Copia de Seguridad
+
+void Agenda::GuardarCopia(){
+	string aux, fichero;
+	ofstream archivo_salida;
+	int tam = tamVect();
+
+	cout << "Escribe el nombre del fichero: " << endl;
+	getline(cin, fichero);
+
+	archivo_salida.open(fichero.c_str(), ios::out);
+
+	for(int i=0; i<tam; i++){
+		archivo_salida << alumnos_[i].getDNI() << "\n";
+		archivo_salida << alumnos_[i].getNombre() << "\n";
+		archivo_salida << alumnos_[i].getApellidos() << "\n";
+		archivo_salida << alumnos_[i].getTelefono() << "\n";
+		archivo_salida << alumnos_[i].getDireccion() << "\n";
+		archivo_salida << alumnos_[i].getEmail() << "\n";
+		archivo_salida << alumnos_[i].getCursoMasAlto() << "\n";
+		archivo_salida << alumnos_[i].getLider() << "\n\n";
+	}
+
+	archivo_salida.close();
+
+	ifstream archivo_entrada;
+	archivo_entrada.open(fichero.c_str(), ios::in);
+
+	ofstream archivo_salida1;
+	archivo_salida1.open("Copia_Seguridad.bin", ios::out | ios::binary);
+
+	while(!archivo_entrada.eof()){
+		getline(archivo_entrada, aux);
+		aux += "\n";
+		archivo_salida1.write(aux.c_str(), aux.size());
+	}
+	archivo_entrada.close();
+	archivo_salida1.close();
+}
+
+void Agenda::CargarCopia(){
+	char aux[250];
+	ifstream archivo_entrada;
+	archivo_entrada.open("Copia_Seguridad.bin", ios::in | ios::binary);
+
+	ofstream archivo_salida;
+	archivo_salida.open("Base_Datos");
+
+	while(!archivo_entrada.eof()){
+		archivo_entrada.read(aux, 250);
+		archivo_salida << aux;
+	}
+	archivo_entrada.close();
+	archivo_salida.close();
+}
