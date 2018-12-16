340730dae9a40495fdd2bf5d44d276bcbfe5c4b3
Jorge Jesús Chaparro Ibarra <jorgejezuh@gmail.com>
2018-12-16 13:00:25 +0100

    Mejoras en Copia de Seguridad

--- CopiaSeguridad.h
+++ CopiaSeguridad.h
@@ -1,6 +1,15 @@
 #ifndef COPIA_SEGURIDAD_H
 #define COPIA_SEGURIDAD_H
 
+#include <iostream>
+#include "alumno.h"
+#include "Agenda.h"
+#include "persona.h"
+#include "funcionesAuxiliares.h"
+#include <fstream>
+#include <string>
+#include <vector>
+
 void GuardarCopia();
 void CargarCopia();
 
